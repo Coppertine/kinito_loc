443665d92a39328f73481b9f7aa3b0a6ba7556dd
Coppertine <37494321+Coppertine@users.noreply.github.com>
2024-03-29 09:28:08 +1000

    Attempt to add `your_world` localizations
    To no avail... if ANYONE can figure out accessing a semi-inactive Scene Tree from an active Scene Tree, please let me know.

--- localization_system/TranslationManager.gd
+++ localization_system/TranslationManager.gd
@@ -18,6 +18,7 @@ var config = {}
 var current_lang = ""
 var config_loaded = false
 var config_node
+var current_scene
 #region Classes
 var KinitoLocalizedText = preload("res://localization_system/AllLocalizedData.gd")
 var kinito_loc = KinitoLocalizedText.new()
@@ -33,7 +34,7 @@ var reset_data = ResetGameData.new()
 func config_handler():
 	var dir = Directory.new()
 	dir.open("user://Mods")
-	if (dir.file_exists("!ModConfiguration.zip")):
+	if (dir.file_exists("ModConfiguration.zip")):
 		while config_loaded == false:
 			if get_parent().has_node("Config_Scene"):
 				config_node = get_parent().get_node("Config_Scene")
@@ -99,6 +100,21 @@ func patch_kinito_dialogue(animation, dialogue, audio):
 					yield(get_tree(), "idle_frame")
 				animation.track_set_key_value(aud_track, n, audio_file)
 				
+func patch_animation_track(animation, track, values):
+	var _t = animation.find_track(track)
+
+	if _t != null and values != null:
+		for n in range(animation.track_get_key_count(_t)):
+			animation.track_set_key_value(_t, n, values[n])
+			
+func get_animation_track(animation, track):
+	var _t = animation.find_track(track)
+	$CanvasLayer/LineEdit3.text = "[ "
+	if _t != null:
+		for n in range(animation.track_get_key_count(_t)):
+			$CanvasLayer/LineEdit3.text += "\"" + animation.track_get_key_value(_t, n) + "\", "
+	$CanvasLayer/LineEdit3.text += " ]"
+				
 func _ready():
 	config = default_config
 	config_handler()
@@ -107,17 +123,15 @@ func _ready():
 	for child_node in $CanvasLayer.get_children():
 		child_node.visible = config["SHOW_DEBUG_CONSOLES"] == "True"
 	current_lang = config["LANGUAGE"]
-	files_loaded = load_translation_files(current_lang)	
+	files_loaded = load_translation_files(str(current_lang))
 	
 func _show_node_paths():
 	var path = $CanvasLayer/LineEdit.text
 	var last_path = ""
-	
-	if get_parent().get_parent().get_node(path) != null and last_path != path:
+	if get_parent().get_parent().has_node(path) and last_path != path:
 		$CanvasLayer/ColorRect/RichTextLabel2.bbcode_text = ""
 		for node in get_parent().get_parent().get_node(path).get_children():
 			$CanvasLayer/ColorRect/RichTextLabel2.bbcode_text += node.name + "\n"
-			
 		last_path = path
 
 func _show_localized():
@@ -128,6 +142,9 @@ func _show_localized():
 		$CanvasLayer/ColorRect2/RichTextLabel2.bbcode_text = kinito_loc.kinito_common_text[path]
 		last_path = path
 
+######
+## Boot Screen and Desktop
+######
 func _patch_computer_app(app_name, localized_name):
 	if get_parent().get_parent().get_node("0").get_child(0).has_node("Aspect/"+app_name):
 		print_log("patching app: " + app_name)
@@ -169,6 +186,7 @@ func _patch_app000():
 			print_log("Detected scene swap, repatching.")
 			patched_desktop = false
 		if !patched_desktop and get_parent().get_parent().get_node("0").get_child_count() > 0 and get_parent().get_parent().get_node("0").get_child(0).has_node("Aspect"):
+			pc_name = get_parent().get_parent().get_node("0").get_child(0).name
 			_patch_computer_app("My_Computer", "PC_APPS_MYCOMPUTER")
 			_patch_computer_app("Settings", "PC_APPS_SETTINGS")
 			_patch_computer_app("The_Internet", "PC_APPS_INTERNET")
@@ -180,7 +198,10 @@ func _patch_app000():
 			_patch_computer_app("OS_Paint", "PC_APPS_PAINT")
 			print_log("Patched desktop app shotcuts")
 			patched_desktop = true
-				
+
+######
+## Kinito
+######
 func _patch_app001():
 	if Tab.data["open"][1] and get_parent().get_parent().has_node("1/MAIN_KINITO/Main/Dio"):
 		var dio : AnimationPlayer = get_parent().get_parent().get_node("1/MAIN_KINITO/Main/Dio")
@@ -193,6 +214,9 @@ func _patch_app001():
 			print_log("Patched kinito's TTS lines")
 			patched_dialogue = true
 
+######
+## Disclaimers
+######
 func _patch_app003():
 	if Tab.data["open"][3] == true:
 		var node_3 = get_parent().get_parent().get_node("3").get_child(0)
@@ -216,6 +240,89 @@ func _patch_app003():
 			if node_3.has_node("Active/ASSET/1/Okayt"):
 				node_3.get_node("Active/ASSET/1/Okayt").text = kinito_loc.kinito_common_text["COMMON_OK"]
 
+######
+## Internet
+######
+var patched_internet = {"loading":false,"home":false,"results":false,"page":false}
+var _sites_enum = ["HISTORY","WIKI","NEWVID","IMG","NEWS","STORE","BLOG"]
+func _patch_app005():
+	if Tab.data["open"][5] == true:
+		## Loading screen
+		if !patched_internet["loading"] and get_parent().get_parent().get_node("5").get_child(0).get_node("Active/Ayo"): # Only found on the launching internet part
+			print_log("Patching internet loading screen")
+			var oc : AnimationPlayer = get_parent().get_parent().get_node("5").get_node("Tab/OC")
+			patch_animation_track(oc.get_animation("0"), "Active/Active/ASSET/Download/Info:bbcode_text", kinito_loc.kinito_common_text["PC_INTERNET_CONNECTING"])
+			get_parent().get_parent().get_node("5").get_node("Tab/Active/RichTextLabel").bbcode_text = kinito_loc.kinito_common_text["PC_INTERNET_WELCOME"]
+			patched_internet["loading"] = true
+		
+		if !patched_internet["results"] and get_parent().get_parent().get_node("5").get_child(0).has_node("Active/ScrollContainer/HBoxContainer/Control2/Control"):
+			if get_parent().get_parent().get_node("5").get_child(0).get_node("Active/ScrollContainer/HBoxContainer/Control2/Control/Results/Tween").is_playing():
+				# We need to recreate the search results stats, although it's mostly cosmetic.. so randomise we do!!
+				# these are copies from the code, ish
+				print_log("Patching search results screen")
+	#			var searchCount = int(rand_range(12000,3000000))
+				var time_range =  rand_range(0.15,7.35)
+				var results_text = kinito_loc.kinito_common_text["PC_INTERNET_RESULTS"].replacen("[count]",add_comma_to_int(get_parent().get_parent().get_node("5").get_child(0).get_node("Active/ScrollContainer/HBoxContainer/Control2/Control").currentCount))
+				results_text = results_text.replacen("[time]",str(time_range).left(3))
+				get_parent().get_parent().get_node("5").get_child(0).get_node("Active/ScrollContainer/HBoxContainer/Control2/Control/Results").bbcode_text = results_text
+				print_log("Results set to: " + get_parent().get_parent().get_node("5").get_child(0).get_node("Active/ScrollContainer/HBoxContainer/Control2/Control/Results").bbcode_text)
+				# We have 8 of these buggers...
+				# Let's just hope we modifiy the non P values before it gets cloned over.
+				var results_list = get_parent().get_parent().get_node("5").get_child(0).get_node("Active/ScrollContainer/HBoxContainer/Control2/Control")
+				print_log("Patching "+ str(_sites_enum.count()) + " Sites")
+				
+				for site_i in range(_sites_enum.count()):
+					print_log("Patching Result #: " + str(site_i))
+					print_log("Patching Result: " + _sites_enum[site_i])
+					var site_num = site_i + 1
+					results_list.get_node(str(site_num) + "/Text").bbcode_text = kinito_loc.kinito_common_text["PC_INTERNET_RESULTS_"+_sites_enum[site_i]+"_TITLE"].replacen("[term]",Vars.get("SearchTerm"))
+					results_list.get_node(str(site_num) + "/Sub").bbcode_text = kinito_loc.kinito_common_text["PC_INTERNET_RESULTS_"+_sites_enum[site_i]+"_DESC"].replacen("[term]",Vars.get("SearchTerm"))
+				for error_num in range(8,13):
+					results_list.get_node("fake_" + str(error_num) + "/Node2D/Text").bbcode_text = kinito_loc.kinito_common_text["PC_INTERNET_RESULTS_ERROR_SUB"]
+					results_list.get_node("fake_" + str(error_num) + "/Sub").bbcode_text = kinito_loc.kinito_common_text["PC_INTERNET_RESULTS_ERROR_DESC"]
+				patched_internet["results"] = true
+			
+		## page
+#		if !patched_internet["page"]:
+#			var current_site = Vars.get("WebURL")
+#			match current_site[1]:
+#				sites["HISTORY"]:
+#					patch_history()
+#				sites["WIKI"]:
+#					patch_wiki()
+#				sites["NEWVID"]:
+#					patch_newvid()
+#			patched_internet["page"] = true
+		
+		if get_parent().get_parent().get_node("5").get_child(0).has_node("Active/Title"):
+			get_parent().get_parent().get_node("5").get_child(0).get_node("Active/Title").text = kinito_loc.kinito_common_text["WINDOW_TITLE_INTERNET"]
+#			blog_post_text.bbcode_text = kinito_loc.kinito_common_text["PC_INTERNET_S0"]
+	else:
+		patched_internet["loading"] = false
+		patched_internet["home"] = false
+		patched_internet["results"] = false
+		patched_internet["page"] = false
+
+func patch_history():
+	pass
+
+func patch_wiki():
+	pass
+
+func patch_blog():
+	var current_tab = get_parent().get_parent().get_node("5").get_child(0)
+	if current_tab.has_node("Active/ScrollContainer/HBoxContainer/Control/ColorRect2/0/B"):
+		var blog_post_text = current_tab.get_node("Active/ScrollContainer/HBoxContainer/Control/ColorRect2/0/B")
+		blog_post_text.bbcode_text = kinito_loc.kinito_common_text["PC_INTERNET_S0"].replacen("[Term]", Vars.get("SearchTerm"))
+		
+	## Blog
+	if current_tab.get_node("Active/ScrollContainer/HBoxContainer/Control/ColorRect2/0/A"):
+		var blog_post_header = get_node("Active/ScrollContainer/HBoxContainer/Control/ColorRect2/0/A")
+		blog_post_header.bbcode_text = kinito_loc.kinito_common_text["PC_INTERNET_S0_HEADER"].replacen("[Term]", Vars.get("SearchTerm"))
+
+######
+## Settings
+######
 func _patch_app007():
 	var patched_button = false
 	if Tab.data["open"][7] and get_parent().get_parent().get_node("7").has_node("Tab/Active"):
@@ -278,25 +385,73 @@ func _patch_app007():
 		finish.text = kinito_loc.kinito_common_text["COMMON_FINISH"]
 		last_save.text = kinito_loc.kinito_common_text["COMMON_LSAVED"] % Data.data["lastSave"]
 
+######
+## Pause Menu
+######
 func _patch_app010():
 	#NROOT/_/Active/Label
 	if Tab.data["open"][10] and get_parent().get_parent().has_node("10/NROOT/_/Active/Label"):
 		var last_save = get_parent().get_parent().get_node("10/NROOT/_/Active/Label")
 		last_save.text = kinito_loc.kinito_common_text["COMMON_LSAVED"] % Data.data["lastSave"]
 
+######
+## Friendship Club Form
+## and Registration
+######
+var patched_friendship_club = false
+var friendship_node_name = ""
+func _patch_app020():
+	if Tab.data["open"][20] == false or !Tab.data["open"][20]:
+		return
+	if patched_friendship_club and get_parent().get_parent().get_node("20").get_child(0): # Instalation
+		# need to change the animation for Active/ASSET/MAIN/T1 at Tab/AnimationPlayer
+		friendship_node_name = get_parent().get_parent().get_node("20").get_child(0).name
+		var form_node = get_parent().get_parent().get_node("20").get_child(0)
+					
+		patched_friendship_club = false
+	if !patched_friendship_club and get_parent().get_parent().get_node("20").get_child(0).has_node("Active/tab"): # Form
+		friendship_node_name = get_parent().get_parent().get_node("20").get_child(0).name
+		var form_node = get_parent().get_parent().get_node("20").get_child(0)
+		form_node.get_node("Active/Title").text = kinito_loc.kinito_common_text["WINDOW_FCLUB_TITLE"]
+		form_node.get_node("Active/Asset/desc").text = kinito_loc.kinito_common_text["FORM_FCLUB_DESC"]
+		form_node.get_node("Active/Asset/BGTEXT/Membership_Re").text = kinito_loc.kinito_common_text["FORM_FCLUB_HEADER"]
+		form_node.get_node("Active/Asset/BGTEXT/BT0").text = kinito_loc.kinito_common_text["FORM_FCLUB_FNAME"]
+		form_node.get_node("Active/Asset/BGTEXT/BT1").text = kinito_loc.kinito_common_text["FORM_FCLUB_LNAME"]
+		form_node.get_node("Active/Asset/BGTEXT/BT2").text = kinito_loc.kinito_common_text["FORM_FCLUB_EMAIL"]
+		form_node.get_node("Active/Asset/BGTEXT/BT3").text = kinito_loc.kinito_common_text["FORM_FCLUB_BDAY"]
+		form_node.get_node("Active/Asset/BGTEXT/BT4").text = kinito_loc.kinito_common_text["FORM_FCLUB_BMONTH"]
+		form_node.get_node("Active/Asset/BGTEXT/BT5").text = kinito_loc.kinito_common_text["FORM_FCLUB_BYEAR"]
+		form_node.get_node("Active/Asset/BGTEXT/BT6").text = kinito_loc.kinito_common_text["FORM_FCLUB_ADD1"]
+		form_node.get_node("Active/Asset/BGTEXT/BT7").text = kinito_loc.kinito_common_text["FORM_FCLUB_ADD2"]
+		form_node.get_node("Active/Asset/BGTEXT/BT8").text = kinito_loc.kinito_common_text["FORM_FCLUB_CITY"]
+		form_node.get_node("Active/Asset/BGTEXT/BT9").text = kinito_loc.kinito_common_text["FORM_FCLUB_COUNTRY"]
+		form_node.get_node("Active/Asset/BGTEXT/BT11").text = kinito_loc.kinito_common_text["FORM_FCLUB_ZIP"]
+		
+		patched_friendship_club = true
+#	if !patched_friendship_club and get_parent().get_parent().get_node("20").get_child(0).has_node(""):
+	pass
+
 func _patch_funfair():
 	# good thing it's only 1 object.. nope..
-	if !patched_funfair and App.data["data"][4] == "FUNFAIR_INITLOAD" and get_tree().current_scene().has_node("Main/FunFair"):
+#	print_log(get_tree().get_current_scene().name)
+	
+#	if App.data["data"][4] == "FUNFAIR_INITLOAD":
+#		print_log("-------------")
+#		print_log(App.current)
+#		for child_node in get_tree().get_root().get_children():
+#			print_log(child_node.name)
+	if !patched_funfair and App.data["data"][4] == "FUNFAIR_INITLOAD" and get_tree().get_current_scene().has_node("Listener/ClearAutoLoad/Main/FunFair"):
 		print_log("Patching funfair dialog")
-		get_tree().current_scene().get_node("Main/FunFair/IntroSeq/TITLE/Welcome/Viewport/Text").text = kinito_loc.kinito_common_text["YWRD_FFAIR_WELTO"]
-		get_tree().current_scene().get_node("Main/FunFair/IntroSeq/TITLE/AnimationPlayer/start").text = "\n" + kinito_loc.kinito_common_text["YWRD_FFAIR_CLICK"]
-		get_tree().current_scene().get_node("Main/FunFair/Funland/StoryArea/[desc] A miniature version of your funfair").name = "[desc] " + kinito_loc.kinito_common_text["YWRD_FFAIR_MINI"]
+		
+		get_tree().get_current_scene().get_node("Main/FunFair/IntroSeq/TITLE/Welcome/Viewport/Text").bbcode_text = "\n[wave amp=-40.0 freq=0.0 connected=1][rainbow freq=0.4 sat=2.0 val=0.9] " +kinito_loc.kinito_common_text["YWRD_FFAIR_WELTO"]
+		get_tree().get_current_scene().get_node("Main/FunFair/IntroSeq/TITLE/AnimationPlayer/start").bbcode_text = "[color=black]\n" + kinito_loc.kinito_common_text["YWRD_FFAIR_CLICK"]
+		get_tree().get_current_scene().get_node("Main/FunFair/Funland/StoryArea/[desc] A miniature version of your funfair").name = "[desc] " + kinito_loc.kinito_common_text["YWRD_FFAIR_MINI"]
 		# Fun fact, the sign in front of the funfair is a text element.. let's change that..
 		# Note, [name] replaces with the chosen player name 
 		# (luckly, one point in the script, 
 		#   there's a check to see if there are more than 4 chars 
 		#   in the name itself and change size of text.)
-		get_tree().current_scene().get_node("Main/FunFair/IntroSeq/TITLE/AnimationPlayer/start").text = kinito_loc.kinito_common_text["YWRD_FFAIR_SIGN"].replace("[name]",Data.data["name"])
+		get_tree().get_current_scene().get_node("Main/FunFair/Funland/Sign/Viewport/Control/T").bbcode_text = "[center]" + kinito_loc.kinito_common_text["YWRD_FFAIR_SIGN"].replace("[name]",Data.data["name"])
 		patched_funfair = true
 		
 func _patch_your_home():
@@ -332,7 +487,7 @@ func _patch_your_home():
 func _patch_notes():
 	Tab.objectives = kinito_loc.kinito_common_text["PC_NOTES"]
 	patched_notes = true
-	
+
 func _patch_email():
 	var exe_path = str(OS.get_executable_path().get_base_dir().replace("\\","/"))
 	var email_names = ["KPET", "NTL", "TTT", "ENTRY", "WAI", "BDOOR", "LWRLD", "GLUCK", "FSC"]
@@ -341,22 +496,25 @@ func _patch_email():
 		print_log("Patching email: "+email_name)
 		var message = kinito_loc.kinito_common_text["EMAIL_"+email_name+"_MESSAGE"]
 		if email_name == "TTT":
-			# Note to self, move this to somehere that the translate can.. place in..
+			# Note to self, move this to somehere that the translators can.. place in..
 			message.replace("%s/extra/article.png", exe_path + "/extra/article.png")
 		emails.append([kinito_loc.kinito_common_text["EMAIL_"+email_name+"_TITLE"],kinito_loc.kinito_common_text["EMAIL_"+email_name+"_SENDER"],message])
 	Tab.email = emails
 	patched_email = true
 	pass
-	
+
+var found_your_world = false
 func _process(delta):
+	
 	# Patch intro screen (PC)
-	_show_node_paths()
+	if config["SHOW_DEBUG_CONSOLES"] == true:
+		_show_node_paths()
 	if files_loaded:
 		if Input.is_key_pressed(KEY_TAB):
 			print_log("Reloading translation files...")
 			files_loaded = load_translation_files(current_lang)
-		
-		_show_localized()
+		if config["SHOW_DEBUG_CONSOLES"] == true:
+			_show_localized()
 		
 		if Data.data["lastSave"] == "never":
 			Data.data["lastSave"] = kinito_loc.kinito_common_text["COMMON_LSAVED_NEVER"]
@@ -367,16 +525,34 @@ func _process(delta):
 		_patch_app000()
 		_patch_app001()
 		_patch_app003()
-		_patch_app007()
+		_patch_app005()
+		_patch_app007() 
 		_patch_app010()
-		if Data.data["sp"] == 12 and Vars.get("PC_Friend_KillAllFINISH") == true:
-			# there's a ton of stuff with these two that revolve around scene trees that i don't want to f over..
-			# So this should ONLY run once Kinito "kills" all of your Steam Friends (used inside the house)
-			# (Around this point, kinito will save said friends into a json file and some png files in '.steam')
+		_patch_app020()
+		# there's a ton of stuff with these two that revolve around scene trees that i don't want to f over..
+		# So this should ONLY run once Kinito "kills" all of your Steam Friends (used inside the house)
+		# (Around this point, kinito will save said friends into a json file and some png files in '.steam')
+		
+		# Also for any mod developers, NEVER RELY ON SP 12. 
+		#  It's set to SP 12 as early as the Permission Privilages chapter
 			
-			# Also for any mod developers, NEVER RELY ON SP 12. 
-			#  It's set to SP 12 as early as the Permission Privilages chapter
-			_patch_funfair()
-			_patch_your_home()
+		########
+		# Funfair and your_home reside in a seperate SceneTree scene.
+		# So far, I am unable to modify a scene from a seperate scene tree instance.
+		# Making localization of your_world near impossible.
+		# If anyone is able to an can do it with as little modification to the scene files themselves, 
+		# I will thank them for the rest of their life..
+		########
+#		_patch_funfair()
+#		_patch_your_home()
 func print_log(line):
 	print("[localization] ", line)
+
+# https://www.croben.com/2020/10/add-commas-on-floats-or-ints-in-gdscript.html
+# Somehow it's what's done in game as well.. noted
+func add_comma_to_int(value: int) -> String:
+	var str_value: String = str(value)
+	var loop_end: int = 0 if value > -1 else 1
+	for i in range(str_value.length()-3, loop_end, -3):
+		str_value = str_value.insert(i, ",")
+	return str_value
